2a2efe8b256695cf9c47f3853aac31c3b8df2367
spriha-srivastava <123316680+spriha-srivastava@users.noreply.github.com>
2023-01-22 21:51:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const currentTempEl = document.getElementById('current-temp');
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-const API_KEY = 'a51217b75085376159aa1c2ffbc54333';
+const API_KEY = '49cc8c821cd2aff9af04c9f98c36eb74';
 
 setInterval(() => {
     const time = new Date();
